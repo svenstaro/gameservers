0f8576a9fcaa36d4ab8b02bff6d8b2ccd8b47ac7
Sven-Hendrik Haase <sh@lutzhaase.com>
2015-04-23 23:54:23 +0000

    Finalize csgo esl config

--- csgo/csgo.sh
+++ csgo/csgo.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-mkdir tmp && cd tmp
-wget http://media.steampowered.com/installer/steamcmd_linux.tar.gz
-wget http://gfx.esl.eu/media/eu/csgo/downloads/configs/csgo_esl_serverconfig.zip
+mkdir tmp
+cd tmp
+wget -c -N \
+    http://media.steampowered.com/installer/steamcmd_linux.tar.gz \
+    http://gfx.esl.eu/media/eu/csgo/downloads/configs/csgo_esl_serverconfig.zip
 tar xf steamcmd_linux.tar.gz
-unzip csgo_esl_serverconfig.zip
+unzip -f csgo_esl_serverconfig.zip
 
 cd ..
 tmp/steamcmd.sh +login anonymous +force_install_dir ../csgoserver +app_update 740 validate +quit
-csgoserver/srcds_run -game csgo -console -usercon +game_type 0 +game_mode 1 +mapgroup mg_active +map de_dust2
+
+cd csgoserver
+./srcds_run -game csgo -console -usercon +game_type 0 +game_mode 1 \
+    +mapgroup mg_active +map de_dust2 -exec ../tmp/esl5on5rl.cfg -tickrate 128 \
+    +hostname "Kuschelmatch" sv_password "lol" rcon_password "hunter2"
